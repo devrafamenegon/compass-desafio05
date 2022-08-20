479e428522d6b628a1973241494a31a9cd4ea847
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-20 00:04:07 -0300

    feat(validation): find all products query params validate
    Add custom middleware.

---
+++ src/app/validations/product/queries.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-invalid-void-type */
+import { NextFunction, Request, Response } from 'express'
+import BadRequestError from '../../errors/BadRequestError'
+
+const acceptedKeys = ['department', 'brand', 'page']
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<Object | void> => {
+  try {
+    const query: Object = req.query
+
+    for (const key in query) {
+      if (!acceptedKeys.includes(key)) {
+        throw new BadRequestError(`${key} is not a valid query parameter`)
+      }
+    }
+  } catch (error) {
+    return res.status(400).json({
+      message: error.name,
+      details: [
+        { message: error.message, acceptedKeys }
+      ]
+    })
+  }
+  return next()
+}
