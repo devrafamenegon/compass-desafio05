060b149ba61646d6eecfb095e030b9150d31da90
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-22 19:22:45 -0300

    Merge branch develop in feature/product-create-by-csv

--- src/routes/product.router.ts
+++ src/routes/product.router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import ProductController from '../app/controller/ProductController'
 import createValidation from '../app/validations/product/create'
 import updateValidation from '../app/validations/product/update'
+import multer from 'multer'
 
 const router = Router()
 
 router.post('/api/v1/product', createValidation, ProductController.create)
-router.get('/api/v1/product', ProductController.findAll)
+router.post('/api/v1/product/csv', multer().single('file', 'csv'), ProductController.createWithCsv)
+router.get('/api/v1/product', validateQuery, ProductController.findAll)
 router.get('/api/v1/product/low_stock', ProductController.findLowStock)
 router.get('/api/v1/product/:id', ProductController.findOne)
 router.put('/api/v1/product/:id', updateValidation, ProductController.update)
