161f347aa1b0f7746be2afcb94be2849d4022108
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-23 10:44:40 -0300

    Merge branch feature/product-create-by-csv in develop

--- src/app/controller/ProductController.ts
+++ src/app/controller/ProductController.ts
@@ -1,3 +1,4 @@
+import { IMulterFile } from '../interfaces/IMulterFile'
 import type { IProductCreate } from 'app/interfaces/IProduct'
 import { Request, Response } from 'express'
 import ProductService from '../service/ProductService'
@@ -84,6 +85,21 @@ class ProductController {
       const { id } = req.params
       const result = await ProductService.delete(id)
       return res.status(204).json(result)
+    } catch (error) {
+      return res.status(error.statusCode ?? 500).json({
+        message: error.name,
+        details: [
+          { message: error.message }
+        ]
+      })
+    }
+  }
+
+  async createWithCsv (req, res): Promise<Response> {
+    try {
+      const file: IMulterFile = req.file
+      const result = await ProductService.createWithCsv(file)
+      return res.status(200).json(result)
     } catch (error) {
       return res.status(error.statusCode ?? 500).json({
         message: error.name,
---
+++ src/app/interfaces/IMulterFile.ts
@@ -0,0 +1,8 @@
+export interface IMulterFile {
+  buffer: Buffer
+  encoding: string
+  fieldname: string
+  mimetype: string
+  originalname: string
+  size: number
+};
