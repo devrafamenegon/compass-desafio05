6fbd884d4b0c29d438c6e4b6182b2edf0f280be4
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-27 00:36:20 -0300

    perf(validate): extract createProductRules and export

--- src/app/validations/product/create.ts
+++ src/app/validations/product/create.ts
@@ -2,19 +2,19 @@
 import { Request, Response, NextFunction } from 'express'
 import Joi from 'joi'
 
+export const createProductRules = Joi.object({
+  title: Joi.string().required().trim(),
+  description: Joi.string().required().trim(),
+  department: Joi.string().required().trim(),
+  brand: Joi.string().required().trim(),
+  price: Joi.number().required().min(0.01).max(1000),
+  qtd_stock: Joi.number().required().min(1).max(100000),
+  bar_codes: Joi.string().required().trim().length(13)
+})
+
 export default async (req: Request, res: Response, next: NextFunction): Promise<Object | void> => {
   try {
-    const schema = Joi.object({
-      title: Joi.string().required().trim(),
-      description: Joi.string().required().trim(),
-      department: Joi.string().required().trim(),
-      brand: Joi.string().required().trim(),
-      price: Joi.number().required().min(0.01).max(1000),
-      qtd_stock: Joi.number().required().min(1).max(100000),
-      bar_codes: Joi.string().required().trim().length(13)
-    })
-
-    const { error } = await schema.validate(req.body, { abortEarly: false })
+    const { error } = await createProductRules.validate(req.body, { abortEarly: false })
     if (error != null) throw error
     return next()
   } catch (error) {
--- src/app/validations/product/createWithCsv.ts
+++ src/app/validations/product/createWithCsv.ts
@@ -1,21 +1,10 @@
-/* eslint-disable @typescript-eslint/no-invalid-void-type */
 import { IProductCreate } from '../../interfaces/IProduct'
-import Joi from 'joi'
+import { createProductRules } from './create'
 
 export default async (payload: IProductCreate): Promise<string[] | string | null> => {
-  const schema = Joi.object({
-    title: Joi.string().required().trim(),
-    description: Joi.string().required().trim(),
-    department: Joi.string().required().trim(),
-    brand: Joi.string().required().trim(),
-    price: Joi.number().required().min(0.01).max(1000),
-    qtd_stock: Joi.number().required().min(1).max(100000),
-    bar_codes: Joi.string().required().trim().length(13)
-  })
-
-  const { error } = await schema.validate(payload, { abortEarly: false })
+  const { error } = await createProductRules.validate(payload, { abortEarly: false })
   const details = error?.details.map((detail) => {
-    return detail.message
+    return detail.message.replace(/"/g, '').replace(/\//g, '')
   })
 
   if (details !== undefined) {
