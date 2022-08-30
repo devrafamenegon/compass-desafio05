e6dd0f0ddd53307acc6793033274e1c7595df3e4
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-08-30 16:36:40 -0300

    feat(app): using morgan middleware

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import routes from './routes/index.router'
 import Database from './infra/database/mongo/index'
 import handleError from './api/middlewares/errorHandler'
+import morganMiddleware from './config/logger'
 
 Database.connect()
 
@@ -21,6 +22,7 @@ class App {
   private middlewares (): void {
     this.server.use(express.json({}))
     this.server.use(express.urlencoded({ extended: true }))
+    this.server.use(morganMiddleware)
   }
 
   private routes (): void {
