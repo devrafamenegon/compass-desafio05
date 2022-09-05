6ff7445019d5b0840e3c894cd33e550277e8647a
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-05 11:24:20 -0300

    refactor(schema): remove dot comma

--- src/api/schemas/UserSchema.ts
+++ src/api/schemas/UserSchema.ts
@@ -15,10 +15,10 @@ const schema = new Schema<IUser>({
 })
 
 schema.pre('save', function(next) {
-  let user = this;
+  let user = this
 
   // only hash the password if it has been modified (or is new)
-  if (!user.isModified('password')) return next();
+  if (!user.isModified('password')) return next()
 
   // generate a salt
   bcrypt.genSalt(SALT ?? 10, function(err, salt) {
@@ -26,20 +26,20 @@ schema.pre('save', function(next) {
 
     // hash the password using our new salt
     bcrypt.hash(user.password, salt, function(err, hash) {
-      if (err) return next(err);
+      if (err) return next(err)
       // override the cleartext password with the hashed one
       user.password = hash
       next()
     })
-  });
-});
+  })
+})
 
 schema.methods.comparePassword = function(candidatePassword, cb) {
   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) return cb(err);
-    cb(null, isMatch);
-  });
-};
+    if (err) return cb(err)
+    cb(null, isMatch)
+  })
+}
 
 const User = mongoose.model<IUser>('User', schema)
 
