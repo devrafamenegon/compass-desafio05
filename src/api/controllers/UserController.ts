9ba238335d5ba0c4622a5440431505bf83f93ef2
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-06 00:22:52 -0300

    feat(controller): add login function

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -3,6 +3,12 @@ import { NextFunction, Request, Response } from 'express'
 import UserService from '../services/UserService'
 
 class UserController {
+  async login (req: Request, res: Response) {
+    const { email, password } = req.body
+    const token = await UserService.login(email, password)
+    return res.status(200).json(token)
+  }
+
   async create (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
     try {
       const payload: IUserCreate = req.body
