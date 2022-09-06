72c2a8a8a81b307702fc59a471a7e6f391b90df1
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-06 20:28:29 -0300

    feat(validation): create user validator

---
+++ src/api/validations/user/create.ts
@@ -0,0 +1,20 @@
+import formatJoiMessage from '../../utils/formatJoiMessage'
+import { Request, Response, NextFunction } from 'express'
+import Joi from 'joi'
+import BadRequest from '../../errors/BadRequestError'
+import { ErrorMessages } from '../../utils/error_messages'
+
+export const createUserRules = Joi.object({
+  email: Joi.string().email().required().trim().max(320),
+  password: Joi.string().required().min(6).max(20).trim()
+})
+
+export default async (req: Request, res: Response, next: NextFunction): Promise<Object | void> => {
+  try {
+    const { error } = await createUserRules.validate(req.body, { abortEarly: false })
+    if (error != null) throw error
+    return next()
+  } catch (error) {
+    return next(new BadRequest(ErrorMessages.BAD_REQUEST, formatJoiMessage(error as Joi.ValidationError) as string ))
+  }
+}
