c05e5b1863eb4de8d7337391827fca71d4bc4516
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-06 00:19:25 -0300

    feat(repository): add find one user and find by email

--- src/api/repositories/UserRepository.ts
+++ src/api/repositories/UserRepository.ts
@@ -1,9 +1,17 @@
 import UserSchema from '../schemas/UserSchema'
-import { IUserCreate, IUserResponse } from '../interfaces/IUser'
+import { IUser, IUserCreate, IUserResponse } from '../interfaces/IUser'
 
 class UserRepository {
   async create (payload: IUserCreate): Promise<IUserResponse> {
     return await UserSchema.create(payload)
+  }
+
+  async findOne (id: string): Promise<IUserResponse | null> {
+    return await UserSchema.findById(id).select('+password')
+  }
+
+  async findOneByEmail (email: string): Promise<IUser | null> {
+    return await UserSchema.findOne({ email }).select('+password')
   }
 }
 
