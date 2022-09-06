19d3b2faad902ff9f00bffaac9403ec15b19cd1f
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-06 00:23:18 -0300

    feat(router): add login user route

--- src/routes/user.router.ts
+++ src/routes/user.router.ts
@@ -1,9 +1,12 @@
 
 import { Router } from 'express'
 import UserController from '../api/controllers/UserController'
+import loginValidation from '../api/validations/user/login'
 
+const prefix = '/user'
 const router = Router()
 
-router.post('/user', UserController.create)
+router.post(prefix, UserController.create)
+router.post(`${prefix}/login`, loginValidation, UserController.login)
 
 export default router
