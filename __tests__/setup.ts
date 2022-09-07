2f83a49ac1c3bafa92e2079bb1f1792d5f0e00f9
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-07 20:04:37 -0300

    feat(test): add cleanDB function in setup

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,13 +1,22 @@
-import ProductSchema from '../src/api/schemas/ProductSchema'
+import mongoose from 'mongoose'
 import Database from '../src/infra/database/mongo/index'
 
+async function cleanDB() {
+  const collecttions = Object.keys(mongoose.connection.collections);
+  await Promise.all(
+    collecttions.map(async (collection) => {
+      await mongoose.connection.collections[collection].deleteMany({});
+    })
+  );
+}
 
 global.beforeAll(async () => {
-  await Database.connect();
-  await ProductSchema.deleteMany({})
+  await Database.connect()
+  await cleanDB()
 })
 
 global.afterAll(async () => {
-  await ProductSchema.deleteMany({})
-  await Database.disconnect();
+  await cleanDB()
+  await Database.disconnect()
+  
 });
