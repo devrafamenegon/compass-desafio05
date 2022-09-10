0f124625cd1b8fbb7a33b0f6594108ac1609d9a9
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-09 23:59:01 -0300

    feat(error_messages): add user_email_duplicated message

--- src/api/utils/error_messages/user.ts
+++ src/api/utils/error_messages/user.ts
@@ -2,6 +2,7 @@ export const UserErrorMessages = {
   TOKEN_NOT_PROVIDED: 'Token not provided',
   TOKEN_NOT_CREATED: 'Token not created',
   TOKEN_INVALID: 'Token invalid',
+  USER_EMAIL_DUPLICATED: 'User email already in use',
   USER_NOT_FOUND: 'User not found',
   USER_NOT_CREATED: 'User not created',
   USER_NOT_UPDATED: 'User not updated',
