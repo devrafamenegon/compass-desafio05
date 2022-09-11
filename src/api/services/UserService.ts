630b4a3a7a16e389acd6700dd966cc288e986729
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-11 20:36:26 -0300

    fix(service): fix import IUserRegister

--- src/api/services/UserService.ts
+++ src/api/services/UserService.ts
@@ -1,5 +1,5 @@
 import UserRepository from "../repositories/UserRepository"
-import { IUserCreate, IUserResponse } from "../interfaces/IUser"
+import { IUserLogin, IUserRegister, IUserResponse } from "../interfaces/IUser"
 import bcrypt from "bcrypt"
 import NotFoundError from "../errors/NotFoundError"
 import { UserErrorMessages } from "../utils/error_messages/user"
@@ -8,19 +8,19 @@ import { createToken } from "../utils/tokenHandler"
 import InternalServerError from "../errors/InternalServerError"
 
 class UserService {
-  async create (payload: IUserCreate): Promise<IUserResponse> {
+  async register (payload: IUserRegister): Promise<IUserResponse> {
     const existingUser = await UserRepository.findOneByEmail(payload.email)
     if (existingUser) throw new BadRequestError(UserErrorMessages.USER_EMAIL_DUPLICATED, 'User with this email already exists')
 
-    const result = await UserRepository.create(payload)
+    const result = await UserRepository.register(payload)
     return result
   }
 
-  async login (email: string, password: string) {
-    const user = await UserRepository.findOneByEmail(email)
+  async login (payload: IUserLogin) {
+    const user = await UserRepository.findOneByEmail(payload.email)
     if (!user) throw new NotFoundError(UserErrorMessages.USER_NOT_FOUND, 'User with this email not found')
     
-    const isPasswordValid = await bcrypt.compare(password, user.password)
+    const isPasswordValid = await bcrypt.compare(payload.password, user.password)
     if (!isPasswordValid) throw new BadRequestError(UserErrorMessages.INVALID_PASSWORD, 'Password or email is invalid')
     
     const token = createToken(user)
