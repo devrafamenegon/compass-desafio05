9746390010a5d5940834a0770cccce5af839439b
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-11 20:37:04 -0300

    fix(controller): remove findAll function

--- src/api/controllers/UserController.ts
+++ src/api/controllers/UserController.ts
@@ -1,28 +1,23 @@
-import { IUserCreate } from '../interfaces/IUser'
+import { IUserRegister, IUserLogin } from '../interfaces/IUser'
 import { NextFunction, Request, Response } from 'express'
 import UserService from '../services/UserService'
 
 class UserController {
-  async login (req: Request, res: Response) {
-    const { email, password } = req.body
-    const token = await UserService.login(email, password)
-    return res.status(200).json(token)
-  }
-
-  async register (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+  async login (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
     try {
-      const payload: IUserCreate = req.body
-      const result = await UserService.register(payload)
-      return res.status(201).json(result)
+    const payload: IUserLogin = req.body
+    const token = await UserService.login(payload)
+    return res.status(200).json(token)
     } catch (error) {
       next(error)
     }
   }
 
-  async findAll (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
+  async register (req: Request, res: Response, next: NextFunction): Promise<Response | undefined> {
     try {
-      const result = await UserService.findAll()
-      return res.status(200).json(result)
+      const payload: IUserRegister = req.body
+      const result = await UserService.register(payload)
+      return res.status(201).json(result)
     } catch (error) {
       next(error)
     }
