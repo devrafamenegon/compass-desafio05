36896785af3303362d439e56cc52304784966c44
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-11 20:35:01 -0300

    fix(interface): remove __v from user interface

--- src/api/interfaces/IUser.ts
+++ src/api/interfaces/IUser.ts
@@ -1,18 +1,24 @@
 export interface IUser {
-  _id?: string
+  _id: string
   email: string
   password: string
+  created_at: Date
+  updated_at: Date
 }
 
 export interface IUserResponse {
   _id: string
   email: string
-  created_at?: Date
-  updated_at?: Date
-  __v?: number
+  created_at: Date
+  updated_at: Date
 }
 
-export interface IUserCreate {
+export interface IUserRegister {
+  email: string
+  password: string
+}
+
+export interface IUserLogin {
   email: string
   password: string
 }
