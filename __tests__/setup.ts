747259c7ba57d9ecbf0eb81acf72e14335fdf0b0
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-11 18:29:17 -0300

    feat(test): add clear database after each

--- __tests__/setup.ts
+++ __tests__/setup.ts
@@ -1,26 +1,17 @@
 import mongoose from 'mongoose'
 import Database from '../src/infra/database/mongo/index'
-import { request } from 'supertest'
+import request from 'supertest'
 import app from '../src/app'
-
-async function cleanDB() {
-  const collecttions = Object.keys(mongoose.connection.collections);
-  await Promise.all(
-    collecttions.map(async (collection) => {
-      await mongoose.connection.collections[collection].deleteMany({});
-    })
-  )
-}
 
 global.beforeAll(async () => {
   await Database.connect()
-  await cleanDB()
+  await Database.clear()
 })
 
-global.afterEach(async () => await cleanDB())
+global.afterEach(async () => await Database.clear())
 
 global.afterAll(async () => {
-  await cleanDB()
+  await Database.clear()
   await Database.disconnect()
 })
 
