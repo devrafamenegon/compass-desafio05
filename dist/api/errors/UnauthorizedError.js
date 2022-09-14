a78eecabbbdde6ad3d6d92a55153a286d86f7575
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-14 13:28:09 -0300

    fix(dist): remove dist from github

--- dist/api/errors/UnauthorizedError.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const CustomError_1 = require("./CustomError");
-class UnauthorizedError extends CustomError_1.CustomError {
-    constructor(message, description) {
-        super(message, description, 'You are unauthorized to access the requested resource. Please log in.', 401);
-    }
-}
-exports.default = UnauthorizedError;
--- dist/api/utils/isValidUuid.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function isValidUuid(id) {
-    const regexUuid = '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$';
-    return id.match(regexUuid) !== null;
-}
-exports.default = isValidUuid;
