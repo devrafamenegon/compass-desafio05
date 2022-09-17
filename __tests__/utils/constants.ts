71ec98dbbe6387e85fcdeb3f4c984cc761becd86
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-17 13:08:08 -0300

    fix(test): use product and user factory in constants

--- __tests__/utils/constants.ts
+++ __tests__/utils/constants.ts
@@ -1,28 +1,18 @@
 import { IProductCreate } from '../../src/api/interfaces/IProduct'
 import { ICreateToken } from '../../src/api/interfaces/IToken'
+import { IUserRegister } from '../../src/api/interfaces/IUser'
 import { createToken } from '../../src/api/utils/tokenHandler'
+import productFactory from './factories/ProductFactory'
+import userFactory from './factories/UserFactory'
 
 export const BASE_URL = '/api/v1'
 
 export const PRODUCT_CSV_FILE = `${__dirname}/files/products.csv`
-
 export const PRODUCT_ENDPOINT = `${BASE_URL}/product`
-export const PRODUCT: IProductCreate = {
-  title: 'Batata Palito',
-  description: 'Batata Palito tradicional 9x9mm congelada pacote 2,5kg - McCain',
-  department: 'Congelados',
-  brand: 'McCain',
-  qtd_stock: 2856,
-  price: 29.54,
-  bar_codes: '6539055340301'
-}
+export const PRODUCT: IProductCreate = productFactory()
 
 export const USER_ENDPOINT = `${BASE_URL}/user`
-
-export const USER = {
-  email: 'rafael.menegon@gmail.com',
-  password: '123456'
-}
+export const USER: IUserRegister = userFactory()
 
 const TOKEN_PAYLOAD: ICreateToken = {
   _id: '97c291f3-b035-427c-a90f-d8ceded729e0',
