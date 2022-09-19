3d2b6539dc41ba0ff26a1cb580741b28638eeb6d
devrafamenegon <dev.rafaelmenegon@gmail.com>
2022-09-18 22:29:01 -0300

    fix(test): check if erro format description is array or not

--- __tests__/utils/formats/ErrorFormat.ts
+++ __tests__/utils/formats/ErrorFormat.ts
@@ -1,7 +1,7 @@
 export const checkErrorFormat = (body): void => {
   expect(body).toEqual({
     message: expect.any(String),
-    description: expect.any(String),
+    description: Array.isArray(body.description) ? expect.any(Array) : expect.any(String),
     http_response: {
       code: expect.any(Number),
       message: expect.any(String),
